7581856d68e9ad821ee883fb439e10a550b235eb
phuongddx <95doanphuong@gmail.com>
2024-06-21 20:51:57 +0700

    macos11

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "SwiftNetworkWrap",
     platforms: [
         .iOS(.v13),
-        .macOS(.v10_15)
+        .macOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
